54bfb7249dac9f89664d08f34594e70cffd3b511
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-14 19:37:10 +0300

    map over the array data

--- app/action.tsx
+++ app/action.tsx
@@ -5,10 +5,16 @@ import { AnimeProp } from "./components/CharacterCard";
 
 export const fetchRickMorty = async () => {
   const response = await fetch("https://rickandmortyapi.com/api/location");
-
   const data = await response.json();
   console.log(data, "data");
 
+  // Check if data is an array
+  if (!Array.isArray(data)) {
+    console.error("Data is not an array:", data);
+    return []; // Return empty array
+  }
+
+  // Map over the array and return an array of CharacterCard components
   return data.map((item: AnimeProp, index: number) => (
     <CharacterCard key={item.id} anime={item} index={index} />
   ));
--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 import React from "react";
 
 export interface AnimeProp {
   id: string;
   name: string;
+  type: string;
+  dimension: string;
   location: string;
-  resident: string;
+  residents: [];
   image: {
     original: string;
   };
@@ -19,5 +23,39 @@ interface Props {
 }
 
 export const CharacterCard = ({ anime, index }: Props) => {
-  return <div>CharacterCard</div>;
+  return (
+    <div className="py-4 m-2  flex flex-col gap-3">
+      <div className="flex justify-between items-center gap-1">
+        <h2 className="font-bold m-5 p-2  text-blue-500 text-md  line-clamp-1 w-full">
+          {anime.type}
+        </h2>
+        <div className="py-1 px-2 bg-[#161921] rounded-sm">
+          <p className="text-white text-sm font-bold capitalize">
+            {anime.dimension}
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-4 items-center">
+        <div className="flex flex-row gap-2 items-center">
+          <Image
+            src="./vercel.svg"
+            alt="episodes"
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+          <p className="text-base text-white font-bold">{anime.residents}</p>
+        </div>
+        <div className="flex flex-row gap-2 items-center">
+          <Image
+            src="./star.svg"
+            alt="star"
+            width={18}
+            height={18}
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
