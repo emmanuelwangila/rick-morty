2531c2fee427c8d5cb43361521719adc064ccc0b
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-14 14:21:11 +0300

    header and footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+function footer() {
+  return (
+    <div className="bg-white rounded-md flex-row ">
+      <h3 className="text-blue-500 ">Emmanuel Wangila</h3>
+      <h4 className="text-blue-500 font-sans text-sm"> All rights reserved</h4>
+    </div>
+  );
+}
