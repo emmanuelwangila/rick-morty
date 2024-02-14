b1e292ec369ef0d7fd551c4182ae26a1cf77cc23
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-14 15:12:31 +0300

    define props for the data

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+export interface AnimeProp {
+  id: string;
+  name: string;
+  location: string;
+  resident: string;
+  status: string;
+  episode: string;
+  charcetr: string;
+}
+
 type Props = {};
 
 export const CharacterCard = (props: Props) => {
