022e65700d0275767db571acbbe607e9111502a8
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-14 14:29:37 +0300

    styling the header

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,3 +8,5 @@ function footer() {
     </div>
   );
 }
+
+export default footer();
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <div className="rounded-md bg-black text-white font-bold">
+      <h1 className="text-white">Rick and Morty App</h1>
+    </div>
+  );
+}
