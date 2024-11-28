f67ce7b392d9605a345555d5b70eb91bf1a65bbb
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-11-28 13:31:43 +0300

    flex jsufy center header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="w-[99%] m-1 mr-2 p-3  rounded-md bg-blue-500 text-white font-bold">
+    <div className="w-[99%] m-1 mr-2 p-3 flex justify-center  rounded-md bg-blue-500 text-white font-bold">
       <h1 className="text-white">Rick and Morty App</h1>
     </div>
   );
